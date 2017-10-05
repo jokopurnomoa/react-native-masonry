65e68945feab93b1b9b1b8a7232ad828c1976f46
Joko Purnomo A <jokopurnomoa@gmail.com>
2017-10-05 13:41:47 +0700

    add touchComponentStyle props

--- components/Masonry.js
+++ components/Masonry.js
@@ -19,135 +19,138 @@ export const assignObjectIndex = (index, targetObject) => ({...targetObject, ...
 const containMatchingUris = (r1, r2) => isEqual(r1.map(brick => brick.uri), r2.map(brick => brick.uri));
 
 export default class Masonry extends Component {
-  static propTypes = {
-    bricks: PropTypes.array,
-    columns: PropTypes.number,
-    sorted: PropTypes.bool,
-    imageContainerStyle: PropTypes.object,
-    customImageComponent: PropTypes.func,
-    customImageProps: PropTypes.object
-  };
-
-  static defaultProps = {
-    bricks: [],
-    columns: 2,
-    sorted: false,
-    imageContainerStyle: {},
-  };
-
-  constructor(props) {
-    super(props);
-    // Assuming users don't want duplicated images, if this is not the case we can always change the diff check
-    this.ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => !containMatchingUris(r1, r2) });
-    this.state = {
-      dataSource: this.ds.cloneWithRows([]),
-      dimensions: {},
-      initialOrientation: true,
-      _sortedData: [],
-      _resolvedData: []
+    static propTypes = {
+        bricks: PropTypes.array,
+        columns: PropTypes.number,
+        sorted: PropTypes.bool,
+        imageContainerStyle: PropTypes.object,
+        touchContainerStyle: PropTypes.object,
+        customImageComponent: PropTypes.func,
+        customImageProps: PropTypes.object
     };
-    // Assuming that rotation is binary (vertical|landscape)
-    Dimensions.addEventListener('change', (window) => this.setState(state => ({ initialOrientation: !state.initialOrientation })))
-  }
 
-  componentDidMount() {
-    this.resolveBricks(this.props);
-  }
+    static defaultProps = {
+        bricks: [],
+        columns: 2,
+        sorted: false,
+        imageContainerStyle: {},
+        touchContainerStyle: {}
+    };
 
-  componentWillReceiveProps(nextProps) {
-    const sameData = containMatchingUris(this.props.bricks, nextProps.bricks);
-    if (sameData) {
-      const differentColumns = this.props.columns !== nextProps.columns;
-
-      if (differentColumns) {
-        const newColumnCount = nextProps.columns;
-        // Re-sort existing data instead of attempting to re-resolved
-        const resortedData = this.state._resolvedData
-          .map((brick, index) => assignObjectColumn(newColumnCount, index, brick))
-          .map((brick, index) => assignObjectIndex(index, brick))
-          .reduce((sortDataAcc, resolvedBrick) => _insertIntoColumn(resolvedBrick, sortDataAcc, this.props.sorted), []);
-
-      	this.setState({
-      	  dataSource: this.state.dataSource.cloneWithRows(resortedData)
-      	});
-      }
-    } else {
-      this.resolveBricks(nextProps);
+    constructor(props) {
+        super(props);
+        // Assuming users don't want duplicated images, if this is not the case we can always change the diff check
+        this.ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => !containMatchingUris(r1, r2) });
+        this.state = {
+            dataSource: this.ds.cloneWithRows([]),
+            dimensions: {},
+            initialOrientation: true,
+            _sortedData: [],
+            _resolvedData: []
+        };
+        // Assuming that rotation is binary (vertical|landscape)
+        Dimensions.addEventListener('change', (window) => this.setState(state => ({ initialOrientation: !state.initialOrientation })))
     }
-  }
 
-  resolveBricks({ bricks, columns }) {
-    bricks
-      .map((brick, index) => assignObjectColumn(columns, index, brick))
-      .map((brick, index) => assignObjectIndex(index, brick))
-      .map(brick => resolveImage(brick))
-      .map(resolveTask => resolveTask.fork(
-        (err) => console.warn('Image failed to load'),
-        (resolvedBrick) => {
-            this.setState(state => {
-              const sortedData = _insertIntoColumn(resolvedBrick, state._sortedData, this.props.sorted);
+    componentDidMount() {
+        this.resolveBricks(this.props);
+    }
 
-              return {
-                dataSource: state.dataSource.cloneWithRows(sortedData),
-                _sortedData: sortedData,
-                _resolvedData: [...state._resolvedData, resolvedBrick]
-              }
-            });;
-        }));
-  }
+    componentWillReceiveProps(nextProps) {
+        const sameData = containMatchingUris(this.props.bricks, nextProps.bricks);
+        if (sameData) {
+            const differentColumns = this.props.columns !== nextProps.columns;
 
-  _setParentDimensions(event) {
-    // Currently height isn't being utilized, but will pass through for future features
-    const {width, height} = event.nativeEvent.layout;
-    this.setState({
-      dimensions: {
-        width,
-        height
-      }
-    });
-  }
+            if (differentColumns) {
+                const newColumnCount = nextProps.columns;
+                // Re-sort existing data instead of attempting to re-resolved
+                const resortedData = this.state._resolvedData
+                    .map((brick, index) => assignObjectColumn(newColumnCount, index, brick))
+                    .map((brick, index) => assignObjectIndex(index, brick))
+                    .reduce((sortDataAcc, resolvedBrick) => _insertIntoColumn(resolvedBrick, sortDataAcc, this.props.sorted), []);
 
-  render() {
-    return (
-  	<View style={{flex: 1}} onLayout={(event) => this._setParentDimensions(event)}>
- 	    <ListView
-         contentContainerStyle={styles.masonry__container}
-         dataSource={this.state.dataSource}
-         enableEmptySections
-         renderRow={(data, sectionId, rowID) =>
-           <Column
-             data={data}
-             columns={this.props.columns}
-             parentDimensions={this.state.dimensions}
-             imageContainerStyle={this.props.imageContainerStyle}
-             customImageComponent={this.props.customImageComponent}
-             customImageProps={this.props.customImageProps}
-             key={`RN-MASONRY-COLUMN-${rowID}`}/> }
-       />
-  	</View>
-    )
-  }
+                this.setState({
+                    dataSource: this.state.dataSource.cloneWithRows(resortedData)
+                });
+            }
+        } else {
+            this.resolveBricks(nextProps);
+        }
+    }
+
+    resolveBricks({ bricks, columns }) {
+        bricks
+            .map((brick, index) => assignObjectColumn(columns, index, brick))
+            .map((brick, index) => assignObjectIndex(index, brick))
+            .map(brick => resolveImage(brick))
+            .map(resolveTask => resolveTask.fork(
+                (err) => console.warn('Image failed to load'),
+                (resolvedBrick) => {
+                    this.setState(state => {
+                        const sortedData = _insertIntoColumn(resolvedBrick, state._sortedData, this.props.sorted);
+
+                        return {
+                            dataSource: state.dataSource.cloneWithRows(sortedData),
+                            _sortedData: sortedData,
+                            _resolvedData: [...state._resolvedData, resolvedBrick]
+                        }
+                    });;
+                }));
+    }
+
+    _setParentDimensions(event) {
+        // Currently height isn't being utilized, but will pass through for future features
+        const {width, height} = event.nativeEvent.layout;
+        this.setState({
+            dimensions: {
+                width,
+                height
+            }
+        });
+    }
+
+    render() {
+        return (
+            <View style={{flex: 1}} onLayout={(event) => this._setParentDimensions(event)}>
+                <ListView
+                    contentContainerStyle={styles.masonry__container}
+                    dataSource={this.state.dataSource}
+                    enableEmptySections
+                    renderRow={(data, sectionId, rowID) =>
+                        <Column
+                            data={data}
+                            columns={this.props.columns}
+                            parentDimensions={this.state.dimensions}
+                            imageContainerStyle={this.props.imageContainerStyle}
+                            touchContainerStyle={this.props.touchContainerStyle}
+                            customImageComponent={this.props.customImageComponent}
+                            customImageProps={this.props.customImageProps}
+                            key={`RN-MASONRY-COLUMN-${rowID}`}/> }
+                />
+            </View>
+        )
+    }
 };
 
 // Returns a copy of the dataSet with resolvedBrick in correct place
 // (resolvedBrick, dataSetA, bool) -> dataSetB
 export function _insertIntoColumn (resolvedBrick, dataSet, sorted) {
-  let dataCopy = dataSet.slice();
-  const columnIndex = resolvedBrick.column;
-  const column = dataSet[columnIndex];
+    let dataCopy = dataSet.slice();
+    const columnIndex = resolvedBrick.column;
+    const column = dataSet[columnIndex];
 
-  if (column) {
-    // Append to existing "row"/"column"
-    const bricks = [...column, resolvedBrick];
-    if (sorted) {
-      // Sort bricks according to the index of their original array position
-      bricks = bricks.sort((a, b) => (a.index < b.index) ? -1 : 1);
+    if (column) {
+        // Append to existing "row"/"column"
+        let bricks = [...column, resolvedBrick];
+        if (sorted) {
+            // Sort bricks according to the index of their original array position
+            bricks = bricks.sort((a, b) => (a.index < b.index) ? -1 : 1);
+        }
+        dataCopy[columnIndex] = bricks;
+    } else {
+        // Pass it as a new "row" for the data source
+        dataCopy = [...dataCopy, [resolvedBrick]];
     }
-    dataCopy[columnIndex] = bricks;
-  } else {
-    // Pass it as a new "row" for the data source
-    dataCopy = [...dataCopy, [resolvedBrick]];
-  }
 
-  return dataCopy;
+    return dataCopy;
 };
